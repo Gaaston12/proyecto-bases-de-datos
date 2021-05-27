4ea3841df4c58538782a62eb18578664c4f6288a
NicoMadariaga <82395567+NicoMadariaga@users.noreply.github.com>
2021-05-27 11:15:48 -0300

    Devolución primer entrega

---
+++ proyecto.sql
@@ -0,0 +1,24 @@
+/*create database Proyecto;
+use proyecto;
+
+create table Alumnos (
+dni int not null ,
+nro int not null auto_increment unique,
+apellido varchar(30) not null,
+nombre varchar(30) not null,
+direccion varchar(30),
+telefono varchar(15),
+primary key (dni)
+);
+ 
+create table Docentes (
+dni int not null ,
+apellido varchar(30) not null,
+nombre varchar(30) not null,
+direccion varchar(30),
+telefono varchar(15),
+dedicacion enum ('exclusivo', 'semi-exclusivo', 'simple'),
+primary key (dni)
+);
+*/
+insert into docentes values (30709655, 'Juan', 'Diaz', 'Rafael oblig','154127531', 'simple');
