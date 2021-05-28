af76bd75dfe5f73403229ece84bb01d6e19eeae9
NicoMadariaga <nicolasmadariaga@gmail.com>
2021-05-27 21:57:14 -0300

    Creadas todas las tablas

--- proyecto.sql
+++ proyecto.sql
@@ -1,6 +1,6 @@
-/*create database Proyecto;
+/*create database Proyecto;*/
 use proyecto;
-
+/*
 create table Alumnos (
 dni int not null ,
 nro int not null auto_increment unique,
@@ -21,8 +21,7 @@ dedicacion enum ('exclusivo', 'semi-exclusivo', 'simple'),
 primary key (dni)
 );
 
-insert into docentes values (30709655, 'Juan', 'Diaz', 'Rafael oblig','154127531', 'simple');*/
-use proyecto;
+insert into docentes values (30709655, 'Juan', 'Diaz', 'Rafael oblig','154127531', 'simple');
 
 create table Materias (
 cod int not null auto_increment,
@@ -31,3 +30,69 @@ dni_responsable int,
 primary key (cod),
 foreign key (dni_responsable) references Docentes (dni)
 )
+
+create table Actividad (
+codigo int not null auto_increment,
+descripcion varchar(50),
+cod_materia int not null,
+primary key (codigo),
+foreign key (cod_materia) references Materias (cod)
+);
+
+create table Realiza (
+dni int not null,
+cod_actividad int not null,
+constraint pkrealiza primary key (dni, cod_actividad),
+foreign key (dni) references Alumnos(dni), 
+foreign key (cod_actividad) references Actividad(codigo)
+);
+
+Create table Resolucion(
+codigo int not null auto_increment,
+nota int,
+fecha date,
+dni_califica int,
+dni_alumno int,
+cod_actividad int,
+primary key (codigo),
+foreign key (dni_califica) references Docentes(dni),
+constraint fkresolucion foreign key (dni_alumno, cod_actividad) references Realiza (dni, cod_actividad)
+);
+
+CREATE TABLE Facultad (
+  codigo int NOT NULL auto_increment,
+  descripcion varchar(45) NOT NULL,
+  PRIMARY KEY (codigo)
+);
+
+CREATE TABLE Cargo (
+  codigo int NOT NULL auto_increment,
+  descripcion varchar(45) NOT NULL,
+  PRIMARY KEY (codigo)
+);
+
+create table Cursa (
+dni int not null,
+cod_materia int not null,
+constraint pkrealiza primary key (dni, cod_materia),
+foreign key (dni) references Alumnos(dni), 
+foreign key (cod_materia) references Materias(cod));
+
+create table Pertenece (
+dni_docente int not null,
+cod_facultad int not null,
+cod_cargo int,
+constraint pkpertenece primary key (dni_docente, cod_facultad),
+foreign key (dni_docente) references Docentes (dni),
+foreign key (cod_facultad) references Facultad (codigo),
+foreign key (cod_cargo) references Cargo (codigo)  
+);
+*/
+create table Equipo (
+dni_docente int not null,
+cod_materia int not null,
+constraint pkequipo primary key (dni_docente, cod_materia),
+foreign key (dni_docente) references Docentes (dni),
+foreign key (cod_materia) references Materias (cod)
+);
+
