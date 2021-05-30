101f2c747dd37ace2642e9df464e8d98b3b58291
Mati36 <lopezmatias36@gamil.com>
2021-05-30 18:27:26 -0300

    Se creo tabla Auditoria y Trigger para ingresar los datos, Se agregaron nuevos datos

--- carga_datos.sql
+++ carga_datos.sql
@@ -1,13 +1,45 @@
+use proyecto;
+
+/*Carga Docentes*/
 insert into docentes values (30709655, 'Juan', 'Diaz', 'Rafael oblig','154127531', 'simple');
-insert into Materias values (null, 'Base de Datoas', 30709654);
+insert into docentes values (134, 'Perez', 'Jose', 'San Martin', '1231', 'simple'); 
+insert into docentes values (135, 'Lopez', 'Jose', 'San Martin', '1232', 'simple'); 
+
+/*Carga Materias*/
+insert into Materias values (null, 'Base de Datos', 30709655);
+
+/*Carga Equipo docentes*/
 insert into Equipo values(30709655, 1);
-insert into docentes values (134, 'Perez', 'Jose', 'San Martin', 1231, 'simple'); 
-insert into Equipo values(30709654, 1);
-insert into docentes values (135, 'Lopez', 'Jose', 'San Martin', 1231, 'simple'); 
-update Materias set dni_responsable = 30709655 where cod = 1;
-insert into Alumnos values (123, null, 'Perez', 'Juan', 'San Martin 111', 111111111);
+insert into Equipo values(134, 1);
+
+/*Carga Alumnos*/
+insert into Alumnos values (123, null, 'Perez', 'Juan', 'San Martin 111', 1501);
+insert into Alumnos values (124, null, 'Fernandez', 'Nicolas', 'San Martin 111', 1505);
+
+/*Carga Actividad*/
 insert into Actividad values (null, 'SQL', 1);
+
+/*Carga Alumnos que realizan actividad*/
 insert into Realiza values (123,1);
-insert Resolucion Values(null, 9, '2021-05-30', 134, 123, 1);
+insert into Realiza values (124,1);
 
+/*Carga Resolucion*/
+insert Resolucion Values(null, 9, current_date(), 134, 123, 1);
+insert Resolucion Values(null, 5, current_date(), 134, 124, 1);
 
+/*El mismo docente modifica la nota del  alumno 123*/
+Update Resolucion 
+	set nota = 7 where (dni_alumno = 123 and cod_actividad = 1);
+Update Resolucion 
+	set nota = 5 where (dni_alumno = 123 and cod_actividad = 1);
+
+/*El mismo docente modifica la nota del alumno 124*/
+Update Resolucion 
+	set nota = 7 where (dni_alumno = 124 and cod_actividad = 1);    
+
+/*Otro docente modifica la nota del alumno 124*/
+Update Resolucion 
+	set nota = 4, dni_califica = 135 where (dni_alumno = 123 and cod_actividad = 1);
+
+/*Muesto aditoria*/
+select * from Auditoria;
--- proyecto.sql
+++ proyecto.sql
@@ -1,127 +1,150 @@
-/*create database Proyecto;*/
+create database if not exists Proyecto;
 use proyecto;
-/*
-create table Alumnos (
-dni int not null ,
-nro int not null auto_increment unique,
-apellido varchar(30) not null,
-nombre varchar(30) not null,
-direccion varchar(30),
-telefono varchar(15),
-primary key (dni)
+
+create table if not exists Alumnos (
+	dni int not null ,
+	nro int not null auto_increment unique,
+	apellido varchar(30) not null,
+	nombre varchar(30) not null,
+	direccion varchar(30),
+	telefono varchar(15),
+	primary key (dni)
 );
  
-create table Docentes (
-dni int not null ,
-apellido varchar(30) not null,
-nombre varchar(30) not null,
-direccion varchar(30),
-telefono varchar(15),
-dedicacion enum ('exclusivo', 'semi-exclusivo', 'simple'),
-primary key (dni)
+create table if not exists Docentes (
+	dni int not null ,
+	apellido varchar(30) not null,
+	nombre varchar(30) not null,
+	direccion varchar(30),
+	telefono varchar(15),
+	dedicacion enum ('exclusivo', 'semi-exclusivo', 'simple'),
+	primary key (dni)
 );
 
-create table Materias (
-cod int not null auto_increment,
-nombre varchar(30),
-dni_responsable int,
-primary key (cod),
-foreign key (dni_responsable) references Docentes (dni)
-)
-
-create table Actividad (
-codigo int not null auto_increment,
-descripcion varchar(50),
-cod_materia int not null,
-primary key (codigo),
-foreign key (cod_materia) references Materias (cod)
+create table if not exists Materias (
+	cod int not null auto_increment,
+	nombre varchar(30),
+	dni_responsable int,
+	primary key (cod),
+	foreign key (dni_responsable) references Docentes (dni)
 );
 
-create table Realiza (
-dni int not null,
-cod_actividad int not null,
-constraint pkrealiza primary key (dni, cod_actividad),
-foreign key (dni) references Alumnos(dni), 
-foreign key (cod_actividad) references Actividad(codigo)
+create table if not exists Actividad (
+	codigo int not null auto_increment,
+	descripcion varchar(50),
+	cod_materia int not null,
+	primary key (codigo),
+	foreign key (cod_materia) references Materias (cod)
 );
 
-Create table Resolucion(
-codigo int not null auto_increment,
-nota float,
-fecha date,
-dni_califica int,
-dni_alumno int not null,
-cod_actividad int not null,
-primary key (codigo),
-foreign key (dni_califica) references Docentes(dni),
-constraint fkresolucion foreign key (dni_alumno, cod_actividad) references Realiza (dni, cod_actividad)
+create table if not exists Realiza (
+	dni int not null,
+	cod_actividad int not null,
+	constraint pkrealiza primary key (dni, cod_actividad),
+	foreign key (dni) references Alumnos(dni), 
+	foreign key (cod_actividad) references Actividad(codigo)
 );
 
-CREATE TABLE Facultad (
-  codigo int NOT NULL auto_increment,
-  descripcion varchar(45) NOT NULL,
-  PRIMARY KEY (codigo)
+create table if not exists Resolucion(
+	codigo int not null auto_increment,
+	nota float,
+	fecha date,
+	dni_califica int,
+	dni_alumno int not null,
+	cod_actividad int not null,
+	primary key (codigo),
+	foreign key (dni_califica) references Docentes(dni),
+	constraint fkresolucion foreign key (dni_alumno, cod_actividad) references Realiza (dni, cod_actividad)
 );
 
-CREATE TABLE Cargo (
-  codigo int NOT NULL auto_increment,
-  descripcion varchar(45) NOT NULL,
-  PRIMARY KEY (codigo)
+create table if not exists Facultad (
+  codigo int not null auto_increment,
+  descripcion varchar(45) not null,
+  primary key (codigo)
 );
 
-create table Cursa (
-dni int not null,
-cod_materia int not null,
-constraint pkrealiza primary key (dni, cod_materia),
-foreign key (dni) references Alumnos(dni), 
-foreign key (cod_materia) references Materias(cod));
-
-create table Pertenece (
-dni_docente int not null,
-cod_facultad int not null,
-cod_cargo int,
-constraint pkpertenece primary key (dni_docente, cod_facultad),
-foreign key (dni_docente) references Docentes (dni),
-foreign key (cod_facultad) references Facultad (codigo),
-foreign key (cod_cargo) references Cargo (codigo)  
+create table if not exists Cargo (
+  codigo int not null auto_increment,
+  descripcion varchar(45) not null,
+  primary key (codigo)
 );
 
-create table Equipo (
-dni_docente int not null,
-cod_materia int not null,
-constraint pkequipo primary key (dni_docente, cod_materia),
-foreign key (dni_docente) references Docentes (dni),
-foreign key (cod_materia) references Materias (cod)
+create table if not exists Cursa (
+	dni int not null,
+	cod_materia int not null,
+	constraint pkrealiza primary key (dni, cod_materia),
+	foreign key (dni) references Alumnos(dni), 
+	foreign key (cod_materia) references Materias(cod)
+);
+
+create table if not exists Pertenece (
+	dni_docente int not null,
+	cod_facultad int not null,
+	cod_cargo int,
+	constraint pkpertenece primary key (dni_docente, cod_facultad),
+	foreign key (dni_docente) references Docentes (dni),
+	foreign key (cod_facultad) references Facultad (codigo),
+	foreign key (cod_cargo) references Cargo (codigo)  
+);
+
+create table if not exists Equipo (
+	dni_docente int not null,
+	cod_materia int not null,
+	constraint pkequipo primary key (dni_docente, cod_materia),
+	foreign key (dni_docente) references Docentes (dni),
+	foreign key (cod_materia) references Materias (cod)
+);
+
+create table if not exists Auditoria(
+	id int not null auto_increment, 
+    id_resoludcion int not null,
+    fecha date,
+    calificacion_anterior float,
+    calificacion_nueva float,
+    dni_docente int not null,
+    constraint pkAuditoria primary key (id),
+    foreign key (id_resoludcion) references Resolucion (codigo),
+    foreign key (dni_docente) references Docentes (dni)
 );
 
 delimiter //
 create trigger alta_docente before insert on Equipo
-FOR EACH ROW
-BEGIN
-	if exists 
-    (Select * from materias 
-	 where (new.cod_materia = Materias.cod) and (new.dni_docente = Materias.dni_responsable)) 
-	then
-		SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'Error: El Docente ya es responsable de la materia';
-	END IF;
-END; //
+	for each row
+	begin
+		if exists 
+		(select * from materias 
+		 where (new.cod_materia = Materias.cod) and (new.dni_docente = Materias.dni_responsable)) 
+		then
+			signal sqlstate '45000'
+			set message_text = 'Error: El Docente ya es responsable de la materia';
+		end if;
+	end; 
+//
 delimiter ;
 
-Drop  trigger alta_docente;
+drop  trigger alta_docente;
 
 delimiter //
 create trigger modificar_responsable before update on Materias
-FOR EACH ROW
-BEGIN
-	if exists 
-    (Select * from Equipo 
-	 where (Equipo.cod_materia = new.cod) and (Equipo.dni_docente = new.dni_responsable)) 
-	then
-		SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'Error: El Docente ya esta en el quipo';
-	END IF;
-END; //
+	for each row
+	begin
+		if exists 
+		(select * from Equipo 
+		 where (Equipo.cod_materia = new.cod) and (Equipo.dni_docente = new.dni_responsable)) 
+		then
+			signal sqlstate '45000'
+			set message_text = 'Error: El Docente ya esta en el quipo';
+		end if;
+	end; 
+//
 delimiter ;
-*/
 
+delimiter //
+create trigger ingreso_auditoria
+    after update on Resolucion 
+    for each row 
+    begin 
+        insert into Auditoria values(null, old.codigo,current_date(), old.nota,new.nota,new.dni_califica);
+    end;  
+//    
+delimiter ;
